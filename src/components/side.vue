32883708620160a3fb80cb88c970e7eb41c1509b
NNNNzs <709934831@qq.com>
2018-09-26 19:04:26 +0800

    9.26下班

--- src/components/side.vue
+++ src/components/side.vue
@@ -38,7 +38,7 @@ export default {
             return this.$store.state.Drawer
         },
         DrawerUrl(){
-            return this.$store.state.DrawerUrl.replace("http","https")
+            return this.$store.state.DrawerUrl;
         }
     }
 }
